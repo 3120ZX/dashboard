d5c2e40084920b9ce93ca538a7c3fbc384f6df70
3120ZX <3120eduardo@gmail.com>
2022-03-30 12:36:11 +0700

    a

--- server.js
+++ server.js
@@ -2,28 +2,24 @@ const express = require('express')
 const app = express()
 //const path = require('path')
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 console.log('Server start/restart')
 console.log('listening to port 8000')
 console.log('')
 
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+//app.use(express.json())
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
 app.get('/dashboard', (req, res) => {
     console.log('Viewing Hello World')
 	res.render('dashboard.html',{text: 'TESTING'})
 })
 
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
 const login = require('./routes/admin_login')
 app.use('/admin_login',login)
-
-const users = require('./routes/users')
-app.use('/users',users)
 
 app.listen(8000)
